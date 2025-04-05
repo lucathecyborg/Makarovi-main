875e26d5e002039819b2200371f568904676de7f
lucathecyborg <lucaborg21a@gmail.com>
2025-04-05 14:16:52 +0200

    "fixed gameOver screen"

--- src/source-code/button.cpp
+++ src/source-code/button.cpp
@@ -8,42 +8,47 @@
 #include "Player.hpp"
 #include "button.hpp"
 
-
-
-Button::Button(int x, int y, int w1, int h1, SDL_Texture *tex1) {
+Button::Button(int x, int y, int w1, int h1, SDL_Texture *tex1)
+{
     tex = tex1;
     hitbox = {x, y, w1, h1};
 }
-Button::Button(int x, int y, int w1, int h1, SDL_Texture *tex1, SDL_Texture *tex2) {
+Button::Button(int x, int y, int w1, int h1, SDL_Texture *tex1, SDL_Texture *tex2)
+{
     tex = tex1;
-    pressedTex=tex2;
+    pressedTex = tex2;
     hitbox = {x, y, w1, h1};
 }
 
-void Button::setPressed(bool status){
+void Button::setPressed(bool status)
+{
     pressed = status;
 }
 
-bool Button::GetPressed(){
+bool Button::GetPressed()
+{
     return pressed;
 }
 
-bool Button::IsPressed() {
+bool Button::IsPressed()
+{
     return pressed;
 }
 
-void Button::render(Render &window, SDL_Rect mouseRect) {
-    if(SDL_HasIntersection(&mouseRect,&hitbox))
-    window.renderTexture1(pressedTex,hitbox);
+void Button::render(Render &window, SDL_Rect mouseRect)
+{
+    if (SDL_HasIntersection(&mouseRect, &hitbox))
+        window.renderTexture1(pressedTex, hitbox);
     else
-    window.renderTexture1(tex,hitbox);
+        window.renderTexture1(tex, hitbox);
 }
 
-void Button::setPosition(int x, int y, int w1, int h1) {
+void Button::setPosition(int x, int y, int w1, int h1)
+{
     hitbox = {x, y, w1, h1};
 }
 
-SDL_Rect *Button::getHitbox() {
+SDL_Rect *Button::getHitbox()
+{
     return &hitbox;
 }
-
